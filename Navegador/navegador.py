7904be45fc365c552e017fa2dd55a38a81c5b72e
FR98 <franciscorosal98@gmail.com>
2017-02-02 23:01:34 -0600

    Avance

--- Navegador/navegador.py
+++ Navegador/navegador.py
@@ -8,4 +8,20 @@ class Navegador(object):
 		new_tab = Tab(url, nombre)
 		self.tabs[nombre] = new_tab
 
-	
+	def modificar_tab(self, url, nombre, tab):
+		del self.tabs[tab] #Arreglar
+		tab_mod = Tab(url, nombre)
+		self.tabs[tab] = tab_mod
+
+	def cerrar(self, tab):
+		del(self.tabs[tab]) #Arreglar
+
+	def vaciar(self):
+		self.tabs.clear()
+
+	def guardar_tabs(self):
+		doc = open("tabs.txt", "w")
+		for i in self.tabs:
+			print(self.tabs[i])
+		doc.close()
+		pass
--- Navegador/prueba.py
+++ Navegador/prueba.py
@@ -2,22 +2,41 @@ from navegador import Navegador
 
 tu_navegador = Navegador()
 
-menu = input("""
+op = "si"
+while op == "si":
+	menu = int(input("""
 	1. Crear nueva tab
 	2. Cambiar URL de un tab
 	3. Cerrar un tab
 	4. Cerrar todos los tabs
 	5. Mostrar mis tabs
 	6. Guardar mis tabs
-	7. Salir
-""")
+	7. Salir 
+"""))
 
+	if menu == 1:
+		nombre = input("Nombre del tab? ")
+		url = input("Url: ")
+		tu_navegador.crear_tab(url, nombre)
+	elif menu == 2:
+		tab = int(input("Cual tab desea modificar? "))
+		nombre = input("Nombre del tab? ")
+		url = input("Url: ")
+		tu_navegador.modificar_tab(url, nombre, tab)
+	elif menu == 3:
+		tab = int(input("Que tab desea cerrar? "))
+		tu_navegador.cerrar(tab)
+	elif menu == 4:
+		pre = input("Esta seguro? ")
+		if pre == "si":
+			tu_navegador.vaciar()
+	elif menu == 5:
+		for i in tu_navegador.tabs:
+			print(tu_navegador.tabs[i])
+	elif menu == 6:
+		tu_navegador.guardar_tabs()
+	elif menu == 7:
+		print("Adios")
+		break
 
-nueva_tab = "si"
-while nueva_tab == "si":
-	nombre = input("Nombre del tab? ")
-	url = input("Url: ")
-	tu_navegador.crear_tab(url, nombre)
-	nueva_tab = input("Desea crear otro tab? ")
-
-print(tu_navegador.tabs)
+	op = input("Desea ver el menu? ")
