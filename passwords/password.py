f9c5a00ae0580b9fb8fb4ee8f7b9c1afff3be1f2
FR98 <franciscorosal98@gmail.com>
2017-02-06 18:51:44 -0600

    Cambios

--- passwords/password.py
+++ passwords/password.py
@@ -3,7 +3,7 @@ import string
 import random
 
 lista = []
-for i in sys.argv: lista.append(i)
+for i in sys.argv: lista.append(i) #lee los argumentos
 
 opciones = int(lista[2])
 doc = open("password.txt", "w")
@@ -12,7 +12,7 @@ while opciones > 0:
 	contraseña = ""
 	largo = int(lista[1])
 	while largo > 0:
-		contraseña += (random.choice(string.ascii_letters + string.digits))		
+		contraseña += (random.choice(string.ascii_letters + string.digits))	#genera la contraseña	
 		largo -= 1
 	opciones -= 1
 	doc.write(contraseña + "\n")
