685eee5c78937ec12b851150bfab51b572e60206
FR98 <francisco-rosal98@hotmail.com>
2017-03-28 11:41:33 -0600

    Avance

---
+++ Django/academico/academico/grados/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from grados.models import Grado
+
+
+class GradoForm(forms.ModelForm):
+	class Meta:
+		model = Grado
+		fields = [
+			'nombre', 
+			'orden']
--- Django/academico/academico/grados/views.py
+++ Django/academico/academico/grados/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from grados.models import Grado
 from estudiantes.models import Estudiante
+from grados.forms import GradoForm
 
 # Create your views here.
 
 def lista_grados(request):
 	grados = Grado.objects.all()
-	return render(request, 'lista_grados.html', {'grados': grados})
+	formulario = GradoForm()
+	return render(request, 'lista_grados.html', {
+		'grados': grados, 
+		'grado_form': formulario})
 
 def detalle_grado(request, grado_pk):
-	grado = Grado.objects.grado_pk()
-	estudiantes = Estudiante.objects.all()
-	return render(request, 'detalle_grado.html', {'grado': grado, 'estudiantes': estudiantes})
+	grado = Grado.objects.get(pk = grado_pk)
+	estudiantes = Estudiante.objects.filter(grado=grado)
+	return render(request, 'detalle_grado.html', {
+		'grado': grado, 
+		'estudiantes': estudiantes})
