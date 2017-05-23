e8603a113d3b08caed2523760eadd14012080243
FR98 <franciscorosal98@gmail.com>
2017-05-23 11:46:45 -0600

    avance cuadrados java

--- Java/Generar cuadrados/scripts.js
+++ Java/Generar cuadrados/scripts.js
@@ -1,7 +1,11 @@
-var inCantidad = document.getElementById("cantidad")
+var inCantidad = document.getElementById("cantidad");
 var selColor = document.getElementById("color");
 var botonGenerar = document.getElementById("generar");
-botonIr.onclick = function() {
-	inCantidad = 
-	selColor.innerHTML = texto;
+var cuadrado = document.getElementById("cuadrado");
+var parrafo = document.getElementById("parrafo");
+botonGenerar.onclick = function() {
+	//cuadrado.innerHTML = selColor.value;
+	document.getElementById("cuadrado").style.background = selColor.value;
+	//parrafo.innerHTML = inCantidad.value;
+
 }
