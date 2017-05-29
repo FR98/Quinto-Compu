f2508d3bf5bc431e692331de778136d83dbe09d7
FR98 <franciscorosal98@gmail.com>
2017-05-29 08:41:37 -0600

    avance histograma

--- Java/Lista de Numeros/scripts.js
+++ Java/Lista de Numeros/scripts.js
@@ -1,18 +1,34 @@
 var inNumeroNuevo = document.getElementById("numeroNuevo");
 var botonIngresar = document.getElementById("ingresar");
-var botonHistograma = document.getElementById("histograma");
 var parrafo = document.getElementById("parrafo");
-var listaNumeros = []
+var listaNumeros = [];
+var botonHistograma = document.getElementById("histograma");
+var rango1 = 0;
+var rango2 = 0;
+var rango3 = 0;
+var rango4 = 0;
+var rango5 = 0;
+var rango6 = 0;
+var rango7 = 0;
+var rango8 = 0;
+var rango9 = 0;
+var rango10 = 0;
 
 botonIngresar.onclick = function() {
 	if (inNumeroNuevo.value <= 100 && inNumeroNuevo.value >=0) {
-		listaNumeros.add(inNumeroNuevo.value)
-		parrafo.innerHTML = listaNumeros.value;	
+		listaNumeros.push(inNumeroNuevo.value);
+		parrafo.innerHTML = listaNumeros;
 	}
 	else{
 		window.alert("Ingrese un numero de 0 a 100");
 	}
-	//cuadrado.innerHTML = selColor.value;
-	//document.getElementById("cuadrado").style.background = selColor.value;
+}
 
+botonHistograma.onclick = function() {
+	for (i = 0; i < listaNumeros.lenght; i++) {
+		if (listaNumeros[i] >= 0 && listaNumeros[i] <= 10){
+			rango1 += 1;
+		}
+		
+	}
 }
