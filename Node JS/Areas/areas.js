29c48a84ca4b971c393a6e80d5ff7f77433c727d
FR98 <francisco-rosal98@hotmail.com>
2017-07-31 18:47:15 -0600

    Funcion calcular areas

--- Node JS/Areas/areas.js
+++ Node JS/Areas/areas.js
@@ -1,3 +1,15 @@
+function calcular(figura){
+	this.areaRectangulo = function(base, altura){
+		return base * altura;
+	}
+	this.areaTriangulo = function(base, altura){
+		return (base * altura)/2;
+	}
+	this.areaCirculo = function(radio){
+		return Math.PI * Math.pow(radio,2);
+	}
+}
+
 //rectangulo = figura 1
 //trianglo = figura 2
 //circulo = figura 3
@@ -5,3 +17,17 @@
 //Ingrese base
 //Ingrese altura
 //Ingrese radio
+
+var figura = 3;
+var base = 2;
+var altura = 5;
+var radio = 4;
+var calcular = new calcular();
+
+if (figura == 1){
+	console.log("El area del rectangulo es: ", calcular.areaRectangulo(base, altura));
+} else if (figura == 2){
+	console.log("El area del triangulo es: ", calcular.areaTriangulo(base, altura));
+} else if (figura == 3){
+	console.log("El area del circulo es: ", calcular.areaCirculo(radio));
+}
