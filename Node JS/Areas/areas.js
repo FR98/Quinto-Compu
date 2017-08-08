3d2506e8c931db3f5ca7d31161da90af7ebae932
FR98 <francisco-rosal98@hotmail.com>
2017-08-07 19:00:20 -0600

    Termino areas

--- Node JS/Areas/areas.js
+++ Node JS/Areas/areas.js
@@ -1,3 +1,5 @@
+var readline = require('readline-sync');
+
 function calcular(figura){
 	this.areaRectangulo = function(base, altura){
 		return base * altura;
@@ -10,24 +12,20 @@ function calcular(figura){
 	}
 }
 
-//rectangulo = figura 1
-//trianglo = figura 2
-//circulo = figura 3
-//Ingrese un numero
-//Ingrese base
-//Ingrese altura
-//Ingrese radio
-
-var figura = 3;
-var base = 2;
-var altura = 5;
-var radio = 4;
+var figura = readline.question("1. Rectangulo" + "\n" + "2. Triangulo" + "\n" + "3. Circulo" + "\n" + "Escoja un numero: ");
 var calcular = new calcular();
 
 if (figura == 1){
+	var base = readline.question("Ingrese la base: ");
+	var altura = readline.question("Ingrese la altura: ");
 	console.log("El area del rectangulo es: ", calcular.areaRectangulo(base, altura));
 } else if (figura == 2){
+	var base = readline.question("Ingrese la base: ");
+	var altura = readline.question("Ingrese la altura: ");
 	console.log("El area del triangulo es: ", calcular.areaTriangulo(base, altura));
 } else if (figura == 3){
+	var radio = readline.question("Ingrese el radio: ");
 	console.log("El area del circulo es: ", calcular.areaCirculo(radio));
+} else {
+	console.log("Opcion Invalida.")
 }
