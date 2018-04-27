bc550b08731861459b264490b5b5b17f64dde658
Francisco Rosal <francisco-rosal98@hotmail.com>
2018-04-26 19:11:50 -0600

    Cambios

--- Django/academico/estudiantes/models.py
+++ Django/academico/estudiantes/models.py
@@ -5,7 +5,7 @@ class Estudiante(models.Model):
 	nombres = models.CharField(max_length = 50)
 	apellidos = models.CharField(max_length = 50)
 	edad = models.IntegerField(default= 0)
-	grado = models.ForeignKey('grados.Grado')
+	grado = models.ForeignKey('grados.Grado', on_delete=models.CASCADE)
 
 	def __str__(self):
 		return "%s %s - %s" % (
